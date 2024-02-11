0f2882d8d82e6231955658b644d9f01bdb034215
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-02-11 08:37:30 +0530

    Refactor tournament page layout and add live stream button

---
+++ app/events/[tournaments]/[eventinfo]/live/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TournamentsPage = () => {
+  return (
+    <div>
+      <iframe
+        src="https://player.twitch.tv/?channel=shyam2173&parent=esport-nexus.vercel.app"
+        scrolling="no"
+        height="378"
+        width="620"
+      ></iframe>
+    </div>
+  );
+};
+
+export default TournamentsPage;
--- app/events/[tournaments]/page.tsx
+++ app/events/[tournaments]/page.tsx
@@ -14,7 +14,7 @@ const TournamentPage = async ({
   const tournaments = await getTournament();
   return (
     <main className="bg-black">
-      <div className="h-[400px]  mb-32">
+      <div className=" mb-32">
         <Image
           src={games.image}
           alt="GamesImages"
@@ -22,6 +22,21 @@ const TournamentPage = async ({
           width={400}
           className="w-full h-[500px]"
         />
+        <div className="text-center pt-4">
+          <h1 className="text-5xl mt-3 ml-10 font-bold">Current Tournaments</h1>
+          {/* create a block for live stream */}
+          <div className="flex justify-center">
+            <div className="w-full h-[100px] mt-4 bg-gray-800 rounded-md">
+              <h1 className="text-4xl text-white pt-1">
+                <Link href="/events/valorant/valorant-clash/live">
+                  <button className="bg-green-500 text-white py-3 px-6 rounded-md mt-2">
+                    Live Stream
+                  </button>
+                </Link>
+              </h1>
+            </div>
+          </div>
+        </div>
         <div className="text-center pt-4">
           <h1 className="text-4xl">Upcoming Tournaments</h1>
           {tournaments?.length > 0 ? (
