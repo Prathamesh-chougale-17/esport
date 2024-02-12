e37d31909d20f484c88131a201f9536d4c097497
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-02-12 17:05:45 +0530

    Update live page with multiple streamers

--- app/events/[tournaments]/[eventinfo]/live/page.tsx
+++ app/events/[tournaments]/[eventinfo]/live/page.tsx
@@ -2,14 +2,31 @@ import React from "react";
 
 const TournamentsPage = () => {
   return (
-    <div>
-      <iframe
-        src="https://player.twitch.tv/?channel=shyam2173&parent=esport-nexus.vercel.app"
-        scrolling="no"
-        height="378"
-        width="620"
-      ></iframe>
-    </div>
+    <main className="bg-black lg:flex text-2xl  justify-evenly">
+      <div>
+        <div className="pt-4">
+          <h1 className="text-violet-700">Prathamesh Chougale</h1>
+        </div>
+        <iframe
+          src="https://player.twitch.tv/?channel=shyam2173&parent=esport-nexus.vercel.app"
+          scrolling="no"
+          height="378"
+          width="620"
+        ></iframe>
+      </div>
+      <div>
+        <div className="pt-4">
+          <h1 className="text-violet-700">Ronak Dagale</h1>
+        </div>
+
+        <iframe
+          src="https://player.twitch.tv/?channel=shyam2173&parent=esport-nexus.vercel.app"
+          scrolling="no"
+          height="378"
+          width="620"
+        ></iframe>
+      </div>
+    </main>
   );
 };
 
